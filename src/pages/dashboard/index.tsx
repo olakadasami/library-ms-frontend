6e47eb5d24b1c589b3d460d1ce79de05c2ace6b8
olakadasami <olakadasamuel@gmail.com>
2024-08-27 06:41:19 +0100

    chore: add info cards to dashboard

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,7 +1,23 @@
+import InfoCard from "@/features/dashboard/InfoCard/InfoCard";
+import { infoCardData } from "@/features/dashboard/InfoCard/infoCardData";
 import React from "react";
 
 function index() {
-  return <div>index</div>;
+  return (
+    <div className="space-y-4 sm:py-4 px-6 relative bg-background">
+      <h1 className="text-4xl font-semibold">
+        Hello <span className="text-primary">Samuel</span>
+      </h1>
+      <main className=" space-y-4">
+        {/* Top containers */}
+        <div className="flex flex-wrap gap-4">
+          {infoCardData.map((item) => (
+            <InfoCard key={item.title} {...item} />
+          ))}
+        </div>
+      </main>
+    </div>
+  );
 }
 
 export default index;
