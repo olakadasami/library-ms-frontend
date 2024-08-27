2512124b43dd01db687c422f18333ecc6c956f0f
olakadasami <olakadasamuel@gmail.com>
2024-08-27 04:08:35 +0100

    chore: add Header to layout

--- src/layouts/DashboardLayout.tsx
+++ src/layouts/DashboardLayout.tsx
@@ -1,13 +1,17 @@
+import Header from "@/components/Header/Header";
 import Nav from "@/components/Nav/Nav";
 import React, { PropsWithChildren } from "react";
 
 function DashboardLayout({ children }: PropsWithChildren) {
   return (
     <div className="flex min-h-screen w-full bg-muted/40 overflow-hidden">
-      <aside className="w-fit p-4 lg:block bg-background ">
+      <aside className="w-fit p-4 bg-background ">
         <Nav />
       </aside>
-      <main className="flex-1 overflow-scroll relative">{children}</main>
+      <main className="flex-1 p-4 overflow-scroll relative">
+        <Header />
+        {children}
+      </main>
     </div>
   );
 }
