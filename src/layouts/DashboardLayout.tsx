19f29c077b8d91e2bb61b715b176027ed8695c36
olakadasami <olakadasamuel@gmail.com>
2024-08-27 03:52:24 +0100

    chore: add nav to layout

--- src/layouts/DashboardLayout.tsx
+++ src/layouts/DashboardLayout.tsx
@@ -1,10 +1,14 @@
+import Nav from "@/components/Nav/Nav";
 import React, { PropsWithChildren } from "react";
 
 function DashboardLayout({ children }: PropsWithChildren) {
   return (
-    <React.Fragment>
-      <main>{children}</main>
-    </React.Fragment>
+    <div className="flex min-h-screen w-full bg-muted/40 overflow-hidden">
+      <aside className="w-fit p-4 lg:block bg-background ">
+        <Nav />
+      </aside>
+      <main className="flex-1 overflow-scroll relative">{children}</main>
+    </div>
   );
 }
 
