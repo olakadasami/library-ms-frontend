c24b2aae946d99b17d9e4e36e26f89a0cb35cbbb
olakadasami <olakadasamuel@gmail.com>
2024-09-10 11:28:34 +0100

    feat: add view all button

--- src/features/dashboard/DashboardList.tsx
+++ src/features/dashboard/DashboardList.tsx
@@ -1,12 +1,16 @@
 import React from "react";
 import TableComponent from "../table/Table";
 import { Button } from "@/components/ui/button";
+import Link from "next/link";
+import { Url } from "next/dist/shared/lib/router/router";
 
 interface TableProps<T> {
   caption: string;
   data: T[];
   action?: () => void;
   actionCaption?: string;
+  listAll?: boolean;
+  listAllHref?: Url;
 }
 
 function DashboardList<T extends object>({
@@ -14,16 +18,25 @@ function DashboardList<T extends object>({
   action,
   caption,
   actionCaption,
+  listAll,
+  listAllHref,
 }: TableProps<T>) {
   return (
     <section className="shadow rounded-xl p-4 space-y-4  bg-background">
       <div className="flex items-center justify-between">
         <h2 className="font-semibold text-2xl">{caption}</h2>
-        {action && (
-          <Button onClick={action} variant={"outline"}>
-            {actionCaption}
-          </Button>
-        )}
+        <div className="flex items-center gap-4">
+          {action && (
+            <Button onClick={action} variant={"outline"}>
+              {actionCaption}
+            </Button>
+          )}
+          {listAll && listAllHref && (
+            <Link href={listAllHref}>
+              <Button variant="outline">View all</Button>
+            </Link>
+          )}
+        </div>
       </div>
 
       <TableComponent data={data} />
